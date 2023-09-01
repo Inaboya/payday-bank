6dd76f53aa9fb1e8c8717b87437e6079317dec2a
Inaboya <momohdavid211@gmail.com>
2023-09-01 14:35:23 +0100

    create folders, install dependencies

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response, NextFunction } from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import dotenv from "dotenv";
+import passport from "passport";
+
+const port = process.env.PORT || 3000;
+
+dotenv.config();
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.use(passport.initialize());
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
