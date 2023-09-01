a498fc4c75318ab075587461208b9993b386040a
Inaboya <momohdavid211@gmail.com>
2023-09-01 15:14:43 +0100

    remove uncomment code

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
 import express, { Request, Response, NextFunction } from "express";
-import path from "path";
-// import cookieParser from "cookie-parser";
-// import logger from "morgan";
 import dotenv from "dotenv";
 import cors from "cors";
 import router from "./routes";
-// import passport from "passport";
 
 const port = process.env.PORT || 3000;
-
-
-
-
-
 
 dotenv.config();
 
@@ -22,10 +13,5 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1", router);
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-
-
-// app.use(passport.initialize());
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
