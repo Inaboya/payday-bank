8ef608a8078ed03aea9486be473c76d83e9bd211
Inaboya <momohdavid211@gmail.com>
2023-09-01 15:13:50 +0100

    use routes

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,25 @@ import path from "path";
 // import cookieParser from "cookie-parser";
 // import logger from "morgan";
 import dotenv from "dotenv";
+import cors from "cors";
+import router from "./routes";
 // import passport from "passport";
 
 const port = process.env.PORT || 3000;
 
-// dotenv.config();
+
+
+
+
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1", router);
 // app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 
